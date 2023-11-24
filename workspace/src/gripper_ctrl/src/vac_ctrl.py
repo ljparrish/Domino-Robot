9f48191c4341c4c7fbe3b7d37d2f925bde2e0017
ljparrish <liamparrish1998@gmail.com>
2023-11-24 00:58:45 -0800

    Updates to pick and place and vacuum gripper
    - added basic self test for vacuum gripper
    - organized code related to pick and place actions

--- workspace/src/gripper_ctrl/src/vac_ctrl.py
+++ workspace/src/gripper_ctrl/src/vac_ctrl.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import rospy
+import time
 from std_msgs.msg import Int8
 from std_msgs.msg import Float32
 
 class VacuumGripper():
     def __init__(self):
-        # Initialize Pressure Data
+        # Parameter Initialization
         self.currentPressure = 0
+        self.pressureThreshold = -25.0
 
         # Setup Publisher and subscriber Objects
         # Pressure Sensor Subscriber
@@ -26,3 +28,30 @@ class VacuumGripper():
     
     def off(self):
         self.vacuumControler.publish(0)
+
+    def isGripping(self):
+        if self.getCurrentPressure() > self.pressureThreshold:
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    # Creates a simple object of VacuumGripper and runs through a basic self test
+    testGripper = VacuumGripper()
+    print('Gripper Object Initialized...')
+    print('Basic Self Test Started')
+    print('Gripper Turning on, ')
+    testGripper.on()
+    time.sleep(5)
+    print('Gripper Turning Off')
+    testGripper.off()
+    print('Testing Pressure Feedback, Attach Item to Gripper')
+    testGripper.on()
+    while not testGripper.isGripping():
+        print(f"Pressure : {testGripper.currentPressure} kPa")
+
+    print(f"Grasp Successful at {testGripper.currentPressure} for threshold of {testGripper.pressureThreshold}")
+    print('Testing Max Vacuum Capability ... ')
+    time.sleep(10)
+    print(f"Max Vacuum Recorded as {testGripper.getCurrentPressure()}")
+    print("Basic Self Test Complete!") 
--- workspace/src/robot_ctrl/src/pickandplace.py
+++ workspace/src/robot_ctrl/src/pickandplace.py
@@ -1,88 +1,143 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+import time
+from gripper_ctrl.src.vac_ctrl import VacuumGripper
 from std_msgs.msg import Header
+from std_msgs.msg import Float32
+from std_msgs.msg import Int8
 from geometry_msgs.msg import PoseStamped
 from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
 from moveit_commander import MoveGroupCommander
 
-def main(pickLocation, placeLocation):
-    # Input definitions:
-    # - pickLocation: tf of pick up location relative to baseframe
-    print(pickLocation)
-    # - placeLocation: tf of place location relative to baseframe
-    print(placeLocation)
+def initalizeIK():
     rospy.wait_for_service('compute_ik')
     rospy.init_node('service_query')
 
     compute_ik = rospy.ServiceProxy('compute_ik',GetPositionIK)
+    group = MoveGroupCommander("right_arm")
+    gripper = VacuumGripper()
+    return compute_ik, group, gripper
 
-    zMoveHeight = 0.2 # Set height above table here during pick and place operations
+def pickDomino(pickPose, group, zHeight, gripper):
+    request = GetPositionIKRequest()
+    request.ik_request.group_name = "right_arm"
+
+    link = "right_hand"
+
+    request.ik_request.ik_link_name = link
+    request.ik_request.pose_stamped.header.frame_id = ...
+
+    # Move to zHeight Above Domino
+    request.ik_request.pose_stamped = pickPose
+    group.set_pose_target(pickPose)
+    group.shift_pose_target([0, 0, 1],zHeight)
+    plan = group.plan()
+    group.execute(plan[1])
+
+    # Move down to Domino
+    group.set_pose_target(pickPose)
+    plan = group.plan()
+    group.execute(plan[1])
+
+    # Pick up Domino
+    gripper.on()
+    while not gripper.isGripping():
+        group.shift_pose_target([0, 0, -1],0.005)
+        plan = group.plan()
+        group.execute(plan[1])
+
+    # Retract From Table
+    group.set_pose_target(pickPose)
+    group.shift_pose_target([0, 0, 1],zHeight)
+    plan = group.plan()
+    group.execute(plan[1])
+
+def placeDomino(placePose, group, zHeight, gripper):
+    request = GetPositionIKRequest()
+    request.ik_request.group_name = "right_arm"
+
+    link = "right_hand"
+
+    request.ik_request.ik_link_name = link
+    request.ik_request.pose_stamped.header.frame_id = ...
+
+    # Move to zHeight Above Domino
+    request.ik_request.pose_stamped = placePose
+    group.set_pose_target(placePose)
+    group.shift_pose_target([0, 0, 1],zHeight)
+    plan = group.plan()
+    group.execute(plan[1])
+
+    # Move down to Domino
+    group.set_pose_target(placePose)
+    plan = group.plan()
+    group.execute(plan[1])
+
+    # Drop Domino
+    gripper.off()
+    time.sleep(2)
+
+    # Retract From Table
+    group.set_pose_target(placePose)
+    group.shift_pose_target([0, 0, 1],zHeight)
+    plan = group.plan()
+    group.execute(plan[1])
+
+def flipDomino(group, gripper):
+    # Predefined Domino Flipper Poses w.r.t Game Mat AR Tag
+    dropPose = PoseStamped()
+    dropPose.header = Header(stamp=rospy.Time.now(), frame_id=...)
+    dropPose.pose.position.x = ...
+    dropPose.pose.position.y = ...
+    dropPose.pose.position.z = ...
+    dropPose.pose.orientation.x = ...
+    dropPose.pose.orientation.y = ...
+    dropPose.pose.orientation.z = ...
+    dropPose.pose.orientation.w = ...
+
+    pickPose = PoseStamped()
+    pickPose.header = Header(stamp=rospy.Time.now(), frame_id=...)
+    pickPose.pose.position.x = ...
+    pickPose.pose.position.y = ...
+    pickPose.pose.position.z = ...
+    pickPose.pose.orientation.x = ...
+    pickPose.pose.orientation.y = ...
+    pickPose.pose.orientation.z = ...
+    pickPose.pose.orientation.w = ...
+
+    request = GetPositionIKRequest()
+    request.ik_request.group_name = "right_arm"
+
+    link = "right_hand"
+
+    request.ik_request.ik_link_name = link
+    request.ik_request.pose_stamped.header.frame_id = ...
+
+    request.ik_request.pose_stamped = dropPose
+    group.set_pose_target(dropPose)
+    plan = group.plan()
+    # Move to the Drop Pose
+    group.execute(plan[1])
+    gripper.off()
+    # Wait Until Pressure equalizes
+    while gripper.getCurrentPressure() < -10.0:
+        pass
+
+    request.ik_request.pose_stamped = pickPose
+    group.set_pose_target(pickPose)
+    plan = group.plan()
+    group.execute(plan[1])
+
+    # Pick up Domino
+    gripper.on()
+    while not gripper.isGripping():
+        group.shift_pose_target([0, 0, -1],0.005)
+        plan = group.plan()
+        group.execute(plan[1])
+
+    # Move Up Away from the Table
+    group.shift_pose_target([0, 0, 1],0.1)
+    plan = group.plan()
+    group.execute(plan[1])
     
-    # Set Pose Arrays
-    # Positions
-    x = np.array([pickLocation.pose.position.x, pickLocation.pose.position.x, pickLocation.pose.position.x, placeLocation.pose.position.x, placeLocation.pose.position.x, placeLocation.pose.position.x])
-    y = np.array([pickLocation.pose.position.y, pickLocation.pose.position.y, pickLocation.pose.position.y, placeLocation.pose.position.y, placeLocation.pose.position.y, placeLocation.pose.position.y])
-    z = np.array([pickLocation.pose.position.z + zMoveHeight, pickLocation.pose.position.z, pickLocation.pose.position.z + zMoveHeight, placeLocation.pose.position.z + zMoveHeight, placeLocation.pose.position.z, placeLocation.pose.position.z + zMoveHeight])
-
-    # Orientations
-    qx = np.array([pickLocation.pose.orientation.x, pickLocation.pose.orientation.x, pickLocation.pose.orientation.x, placeLocation.pose.orientation.x, placeLocation.pose.orientation.x, placeLocation.pose.orientation.x])
-    qy = np.array([pickLocation.pose.orientation.y, pickLocation.pose.orientation.y, pickLocation.pose.orientation.y, placeLocation.pose.orientation.y, placeLocation.pose.orientation.y, placeLocation.pose.orientation.y])
-    qz = np.array([pickLocation.pose.orientation.z, pickLocation.pose.orientation.z, pickLocation.pose.orientation.z, placeLocation.pose.orientation.z, placeLocation.pose.orientation.z, placeLocation.pose.orientation.z])
-    qw = np.array([pickLocation.pose.orientation.w, pickLocation.pose.orientation.w, pickLocation.pose.orientation.w, placeLocation.pose.orientation.w, placeLocation.pose.orientation.w, placeLocation.pose.orientation.w])
-
-    while not rospy.is_shutdown():
-        for i in range(len(x)):
-            print("Planning Move Segment: ",i)
-            request = GetPositionIKRequest()
-            request.ik_request.group_name = "right_arm"
-
-            link = "right_hand"
-
-            request.ik_request.ik_link_name = link
-            request.ik_request.pose_stamped.header.frame_id = "base"
-
-            request.ik_request.pose_stamped.pose.position.x = x[i]
-            request.ik_request.pose_stamped.pose.position.y = y[i]
-            request.ik_request.pose_stamped.pose.position.z = z[i]
-            request.ik_request.pose_stamped.pose.orientation.x = qx[i]
-            request.ik_request.pose_stamped.pose.orientation.y = qy[i]
-            request.ik_request.pose_stamped.pose.orientation.z = qz[i]
-            request.ik_request.pose_stamped.pose.orientation.w = qw[i]
-
-            try:
-                response = compute_ik(request)
-                print(response)
-
-                group = MoveGroupCommander("right_arm")
-                group.set_pose_target(request.ik_request.pose_stamped.pose)
-
-                plan = group.plan()
-
-                user_input = input("Does the Trajectory Look Safe? [Y/N]")
-
-                if user_input == 'Y':
-                    group.execute(plan[1])
-            except rospy.ServiceException as e:
-                print("Error: ",e)
-
-
-if __name__ == '__main__':
-    Pose1 = PoseStamped()
-    Pose1.pose.position.x = 0.75
-    Pose1.pose.position.y = 0.291
-    Pose1.pose.position.z = 0.03
-    Pose1.pose.orientation.x = 0.0
-    Pose1.pose.orientation.y = 1.0
-    Pose1.pose.orientation.z = 0.0
-    Pose1.pose.orientation.w = 0.0
-
-    Pose2 = PoseStamped()
-    Pose2.pose.position.x = 0.79
-    Pose2.pose.position.y = -0.114
-    Pose2.pose.position.z = 0.03
-    Pose2.pose.orientation.x = 0.0
-    Pose2.pose.orientation.y = 1.0
-    Pose2.pose.orientation.z = 0.0
-    Pose2.pose.orientation.w = 0.0
-    main(Pose1,Pose2)
