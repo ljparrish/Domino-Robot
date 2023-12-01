309e1c0453f8f0e6974dd88879c0a6534741336c
ljparrish <liamparrish1998@gmail.com>
2023-11-30 16:28:20 -0800

    Image_to_world update

--- workspace/src/domino_vision_pkg/src/image_to_world.py
+++ workspace/src/domino_vision_pkg/src/image_to_world.py
@@ -13,7 +13,7 @@ class Image_to_world:
         self.fy = 624.580994 #pixels
         self.ox = 0.0
         self.oy = 0.0
-        self.dom_sub = rospy.Subscriber("/image_info", game_state, self.image_to_world)
+        self.dom_sub = rospy.Subscriber("/image_info", image_info, self.image_to_world)
         rospy.spin()
 
 
@@ -37,9 +37,9 @@ class Image_to_world:
         num_dots2 = msg.num_dots_half2
         orientation = msg.orientation
         X,Y,Z = self.pixel_to_point(u,v)
-        self.world_pub = rospy.Publisher('/board_info',world_coordinates, queue_size = 10)
+        self.world_pub = rospy.Publisher('/board_info',position_state, queue_size = 10)
         r = rospy.Rate(10)      
-        pub_string = world_coordinates(x = X, y = Y, z = Z, num_dots_half1 = num_dots1, num_dots_half2 = num_dots2, orientation = orientation)
+        pub_string = position_state(x = X, y = Y, z = Z, num_dots_half1 = num_dots1, num_dots_half2 = num_dots2, orientation = orientation)
         self.world_pub.publish(pub_string)
         r.sleep()
 
